a9ca543e581c51933b1aaf071285d89b94a14839
Thomas Crowe <wcrowe@g.clemson.edu>
2022-05-21 10:49:38 -0400

    Worked through part 2

--- index.js
+++ index.js
@@ -13,11 +13,13 @@
 // document.body.append(pineTree)
 
 
-//Bonus-Adding Background
+//Global Variables
 let horizonLine = window.innerHeight / 2
 let sky = window.innerHeight - horizonLine
 let grass = horizonLine
+let inventory
 
+//Adding Background
 function tile(url, left, bottom, width, height){
     for(let y = 0; y < height; y++){
         for(let x = 0; x < width; x++){
@@ -41,6 +43,35 @@ function newImage(file, left, bottom) {
     return item
 }
 
+//Function to add clickable items and place them in Inventory
+function newItem(url, left, bottom){
+    let item = newImage(url, left, bottom)
+    item.addEventListener('dblclick', () => {
+        item.remove()
+        let inventoryItem = document.createElement('img')
+        inventoryItem.src = url
+        inventory.append(inventoryItem)
+    })
+}
+
+//Adding Inventory to page
+function newInventory(){
+    inventory = document.createElement('div')
+    inventory.style.position = 'fixed'
+    inventory.style.bottom = '0px';
+    inventory.style.left = '0px'
+    inventory.style.width = '100%'
+    inventory.style.height = '80px'
+    inventory.style.display = 'flex'
+    inventory.style.flexDirection = 'row'
+    inventory.style.alignItems = 'center'
+    inventory.style.justifyContent = 'space-evenly'
+    inventory.style.border = '2px solid black'
+    inventory.style.backgroundColor = 'brown'
+    document.body.append(inventory)
+}
+
+
 //Adding images to page
 newImage('assets/green-character.gif', 100, 100)
 newImage('assets/pine-tree.png', 450, 200)
@@ -49,18 +80,10 @@ newImage('assets/pillar.png', 350, 100)
 newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
 
-
-//Function to add clickable items
-function newItem(url, left, bottom){
-    let item = newImage(url, left, bottom)
-
-    item.addEventListener('dblclick', () => {
-        item.remove()
-    })
-}
-
 //Adding clickable items
 newItem('assets/sword.png', 500, 405)
 newItem('assets/sheild.png', 165, 185)
 newItem('assets/staff.png', 600, 100)
 
+//Adding Inventory
+newInventory()
